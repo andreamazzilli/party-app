43665b7a07b745ea08d1d2c5364a91ba8694e161
Private <private@Andreas-MacBook-Pro.local>
2019-02-07 23:13:58 +0100

    Updates

---
+++ precache-manifest.2abb88a67af41b8d69f5df8bb16f6d71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbb4c6336a4e647e06d1",
+    "url": "/party-app/static/js/runtime~main.cbb4c633.js"
+  },
+  {
+    "revision": "990749e23cd88e8d2f23",
+    "url": "/party-app/static/js/main.990749e2.chunk.js"
+  },
+  {
+    "revision": "b5e0989510ee03224291",
+    "url": "/party-app/static/js/1.b5e09895.chunk.js"
+  },
+  {
+    "revision": "990749e23cd88e8d2f23",
+    "url": "/party-app/static/css/main.5650ed70.chunk.css"
+  },
+  {
+    "revision": "a92235eda400cdddab2ec514d8517536",
+    "url": "/party-app/index.html"
+  }
+];
