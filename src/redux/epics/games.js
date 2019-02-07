7669b5a1256e48a8f730ffbee1983de856e9b77f
Private <private@Andreas-MacBook-Pro.local>
2019-02-07 23:40:49 +0100

    fix

--- src/redux/epics/games.js
+++ src/redux/epics/games.js
@@ -5,15 +5,17 @@ import * as audio from '../../helpers/play'
 const GAMES_PRELOAD_RESOURCES = ( action$, store ) => action$.pipe(
   ofType( 'GAMES_PRELOAD_RESOURCES' ),
   tap( action => {
-    const host = window.location.href.replace('/index.html','')
+    let path = window.location.pathname
+    path === '/' ? path = '' : path = path
+    console.log( path )
     return store.value.games.toJS().map( el => {
       return el.songs.map( song => {
         console.log( song.url )
-        audio.preloadSong( song.url, () => { action.payload.dispatch( {
+        audio.preloadSong( path + song.url, () => { action.payload.dispatch( {
           type: 'PLAY_SONG_PRELOAD_COMPLETE',
-          payload: song.url
+          payload: path + song.url
         }) } )
-        return song.url
+        return path + song.url
       })
     })
   }),
